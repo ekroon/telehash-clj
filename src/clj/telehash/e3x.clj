b037e88182bc0859a58d9eb310ecc1d7dc62bc2a
Raymond Kroon <raymond@k3n.nl>
2015-04-19 21:43:36 +0200

    load cs1a backing key from key,secret pair

--- src/clj/telehash/e3x.clj
+++ src/clj/telehash/e3x.clj
@@ -1,11 +1,17 @@
 (ns telehash.e3x
+  (:refer-clojure :exclude [key])
   (:import [java.security KeyPairGenerator SecureRandom]
            [org.bouncycastle.asn1.sec SECNamedCurves]
+           [org.bouncycastle.crypto AsymmetricCipherKeyPair]
            [org.bouncycastle.crypto.generators ECKeyPairGenerator]
-           [org.bouncycastle.crypto.params ECDomainParameters ECKeyGenerationParameters]
-           [org.bouncycastle.jcajce.provider.asymmetric.ec BCECPublicKey]
-           [org.bouncycastle.jce.spec ECParameterSpec]
+           [org.bouncycastle.crypto.params ECPublicKeyParameters ECPrivateKeyParameters
+            ECDomainParameters ECKeyGenerationParameters]
            [org.apache.commons.codec.binary Hex]))
+
+(defmulti generate-cs identity)
+(defmulti load-cs (fn [id _] id))
+(defmulti key :id)
+(defmulti secret :id)
 
 (java.security.Security/addProvider (org.bouncycastle.jce.provider.BouncyCastleProvider.))
 
@@ -22,20 +28,31 @@
     (.init generator params)
     generator))
 
-(defmulti generate :id)
-
 (defn make-n-bytes [bytes n]
   (let [actual-n (count bytes)]
     (if (< actual-n n)
       (byte-array (concat (repeat (- n actual-n) 0x00) bytes))
       (byte-array (take-last n bytes)))))
 
-(defmethod generate "1a" [cs]
+(defmethod generate-cs "1a" [cs]
   (let [generator (generator secp160r1-domain)
-        keypair (.generateKeyPair generator)
-        private (-> keypair .getPrivate .getD .toByteArray
-                    (make-n-bytes 20) Hex/encodeHexString)
-        public (-> keypair .getPublic .getQ (.getEncoded true) Hex/encodeHexString)]
-    {:key public
-     :secret private})
+        keypair (.generateKeyPair generator)]
+    {:id "1a" :keypair keypair}))
+
+(defmethod load-cs "1a" [_ {:keys [key secret]}]
+  (let [public (Hex/decodeHex (char-array key))
+        public-Q (-> (.getCurve secp160r1-curve) (.decodePoint public))
+        public-params (ECPublicKeyParameters. public-Q secp160r1-domain)
+        private (Hex/decodeHex (char-array secret))
+        private-D (BigInteger. private)
+        private-params (ECPrivateKeyParameters. private-D secp160r1-domain)
+        keypair (AsymmetricCipherKeyPair. public-params private-params)]
+    {:id "1a" :keypair keypair})
   )
+
+(defmethod key "1a" [cs]
+  (-> (:keypair cs) .getPublic .getQ (.getEncoded true) Hex/encodeHexString))
+
+(defmethod secret "1a" [cs]
+  (-> (:keypair cs) .getPrivate .getD .toByteArray
+                    (make-n-bytes 20) Hex/encodeHexString))
--- test/clj/telehash/e3x_test.clj
+++ test/clj/telehash/e3x_test.clj
@@ -1,14 +1,31 @@
 (ns telehash.e3x-test
+  (:refer-clojure :exclude [key])
   (:require [telehash.e3x :refer :all]
             [clojure.test :refer :all]))
 
-(def cs1a {:id "1a"})
+(def cs1a "1a")
+(def cs1a-test-1 {:key "03be277f53630a084de2f39c7ff9de56c38bb9d10c"
+                  :secret "792fd655c8e03ae16e0e49c3f0265d04689cbea3"})
+
+(def cs1a-test-2 {:key "0365694904381c00dfb7c01bb16b0852ea584a1b0b"
+                  :secret "031b502b0743b80c1575f4b459792b5d76ad636d"})
 
 (defn validate-generated-cs1a [keypair]
-  (is (= 42 (-> keypair :key count)))
-  (is (= 40 (-> keypair :secret count))  (str "SECRET: " (:secret keypair))))
+  (is (= 42 (-> keypair key count)))
+  (is (= 40 (-> keypair secret count))  (str "SECRET: " (:secret keypair))))
 
-(deftest generate-cs1a
-  (doseq [generated (repeatedly 100 #(generate cs1a))]
+(deftest generated-cs1a-key-secret-validation
+  (doseq [generated (repeatedly 100 #(generate-cs cs1a))]
     (validate-generated-cs1a generated))
   )
+
+(deftest generated-cs1a-id-check
+  (is (= "1a" (:id (generate-cs "1a")))))
+
+(deftest valid-cs1a-should-load-correctly
+  (let [cs-1 (load-cs "1a"  cs1a-test-1)
+        cs-2 (load-cs "1a" cs1a-test-2)]
+    (is (= (key cs-1) (:key cs1a-test-1)))
+    (is (= (secret cs-1) (:secret cs1a-test-1)))
+    (is (= (key cs-2) (:key cs1a-test-2)))
+    (is (= (secret cs-2) (:secret cs1a-test-2)))))
