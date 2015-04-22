71065a6cfb97982c6e8bb4506e602a22906d3c28
Raymond Kroon <raymond@k3n.nl>
2015-04-22 22:21:54 +0200

    cs1a: encrypt and decrypt. Bit of spaghetti.

--- src/clj/telehash/e3x.clj
+++ src/clj/telehash/e3x.clj
@@ -1,19 +1,78 @@
 (ns telehash.e3x
   (:refer-clojure :exclude [key])
-  (:import [java.security KeyPairGenerator SecureRandom]
+  (:import [java.nio ByteBuffer]
+           [java.security SecureRandom]
+           [javax.crypto Cipher]
+           [javax.crypto.spec SecretKeySpec IvParameterSpec]
+           [org.apache.commons.codec.binary Hex]
            [org.bouncycastle.asn1.sec SECNamedCurves]
            [org.bouncycastle.crypto AsymmetricCipherKeyPair]
+           [org.bouncycastle.crypto.agreement ECDHBasicAgreement]
+           [org.bouncycastle.crypto.digests SHA256Digest]
            [org.bouncycastle.crypto.generators ECKeyPairGenerator]
+           [org.bouncycastle.crypto.macs HMac]
            [org.bouncycastle.crypto.params ECPublicKeyParameters ECPrivateKeyParameters
-            ECDomainParameters ECKeyGenerationParameters]
-           [org.apache.commons.codec.binary Hex]))
+            ECDomainParameters ECKeyGenerationParameters KeyParameter]
+           [org.bouncycastle.jcajce.provider.digest SHA256$Digest SHA256$HashMac]))
 
-(defmulti generate-cs identity)
-(defmulti load-local (fn [id _] id))
+(java.security.Security/addProvider (org.bouncycastle.jce.provider.BouncyCastleProvider.))
+
+(defmulti generate-local identity)
+(defmulti load-local (fn [cs-id _] cs-id))
+(defmulti generate-ephemeral identity)
+(defmulti load-remote (fn [cs-id _] cs-id))
 (defmulti key :id)
 (defmulti secret :id)
 
-(java.security.Security/addProvider (org.bouncycastle.jce.provider.BouncyCastleProvider.))
+(defmulti decrypt (fn [local msg] (:id local)))
+(defmulti encrypt (fn [local remote msg] (:id local)))
+
+(defn bytes->hex [#^bytes bytes]
+  (Hex/encodeHexString bytes))
+
+(defn hex->bytes [^String hex]
+  (Hex/decodeHex (char-array hex)))
+
+(defn int->bytes [x]
+  (let [buffer (ByteBuffer/allocate 4)]
+    (.putInt buffer x)
+    (.array buffer)))
+
+(defn bytes->SHA256 [#^bytes bytes]
+  (let [digester (SHA256$Digest.)]
+    (.engineUpdate digester bytes 0 (count bytes))
+    (.engineDigest digester)))
+
+(defn HMAC-SHA256 [key & values]
+  (let [hmac (HMac. (SHA256Digest.))
+        result (byte-array 32)]
+    (.init hmac (KeyParameter. key))
+    (doseq [v values]
+      (.update hmac v 0 (count v)))
+    (.doFinal hmac result 0)
+    result))
+
+(defn AES-128-CTR-encrypt [keybuf ivbuf #^bytes bytes]
+  (let [cipher (Cipher/getInstance "AES/CTR/NoPadding")
+        key (SecretKeySpec. keybuf "AES")
+        iv (IvParameterSpec. ivbuf)]
+    (.init cipher Cipher/ENCRYPT_MODE key iv)
+    (.doFinal cipher bytes)))
+
+(defn AES-128-CTR-decrypt [keybuf ivbuf #^bytes bytes]
+  (let [cipher (Cipher/getInstance "AES/CTR/NoPadding")
+        key (SecretKeySpec. keybuf "AES")
+        iv (IvParameterSpec. ivbuf)]
+    (.init cipher Cipher/DECRYPT_MODE key iv)
+    (.doFinal cipher bytes)))
+
+(defn fold
+  ([bytes] (fold bytes 1))
+  ([bytes n]
+   (if (or (>= 0 n) ((complement even?) (count bytes))) bytes
+       (let [[p1 p2] (split-at (/ (count bytes) 2) bytes)
+             folded (byte-array (map bit-xor p1 p2))]
+         (recur folded (- n 1))))))
 
 (def ^:static secp160r1-curve (SECNamedCurves/getByName "secp160r1"))
 (def ^:static secp160r1-domain
@@ -22,7 +81,7 @@
                        (.getN secp160r1-curve)
                        (.getH secp160r1-curve)))
 
-(defn- generator [domain]
+(defn keypair-generator [domain]
   (let [generator (ECKeyPairGenerator.)
         params (ECKeyGenerationParameters. domain (SecureRandom.))]
     (.init generator params)
@@ -34,28 +93,92 @@
       (byte-array (concat (repeat (- n actual-n) 0x00) bytes))
       (byte-array (take-last n bytes)))))
 
-(defmethod generate-cs "1a" [cs]
-  (let [generator (generator secp160r1-domain)
+(defn load-private-secp160r1 [private]
+  (let [D (BigInteger. private)]
+    (ECPrivateKeyParameters. D secp160r1-domain)))
+
+(defn load-public-secp160r1 [public]
+  (let [Q (-> (.getCurve secp160r1-curve) (.decodePoint public))]
+    (ECPublicKeyParameters. Q secp160r1-domain)))
+
+(defn secp160r1-shared-secret [private public]
+  (let [Q (-> (.getCurve secp160r1-curve) (.decodePoint public))
+        D (BigInteger. private)
+        agreement (ECDHBasicAgreement.)]
+    (.init agreement (ECPrivateKeyParameters. D secp160r1-domain))
+    (-> (.calculateAgreement agreement (ECPublicKeyParameters. Q secp160r1-domain))
+        .toByteArray)))
+
+(defmethod generate-local "1a" [_]
+  (let [generator (keypair-generator secp160r1-domain)
         keypair (.generateKeyPair generator)]
-    {:id "1a" :keypair keypair}))
+    {:id "1a" :public (.getPublic keypair) :private (.getPrivate keypair)}))
 
 (defmethod load-local "1a" [_ {hex-key :key  hex-secret :secret}]
   (let [public (Hex/decodeHex (char-array hex-key))
-        public-Q (-> (.getCurve secp160r1-curve) (.decodePoint public))
-        public-params (ECPublicKeyParameters. public-Q secp160r1-domain)
         private (Hex/decodeHex (char-array hex-secret))
-        private-D (BigInteger. private)
-        private-params (ECPrivateKeyParameters. private-D secp160r1-domain)
-        keypair (AsymmetricCipherKeyPair. public-params private-params)
-        result {:id "1a" :keypair keypair}]
-    (if-not (= hex-key (key result)) (throw (Exception. "invalid key")))
-    (if-not (= hex-secret (secret result)) (throw (Exception. "invalid secret")))
+        private-key (load-private-secp160r1 private)
+        public-key (load-public-secp160r1 public)
+        result {:id "1a" :public public-key :private private-key}]
+    (if-not (= hex-key (-> result key bytes->hex)) (throw (Exception. "invalid key")))
+    (if-not (= hex-secret (-> result secret bytes->hex)) (throw (Exception. "invalid secret")))
     result)
   )
 
+(defmethod generate-ephemeral "1a" [_]
+  (let [remote (generate-local "1a")]
+    remote))
+
+(defmethod load-remote "1a" [_ {hex-key :key}]
+  (let [endpoint (Hex/decodeHex (char-array hex-key))
+        ephemeral (generate-ephemeral "1a")
+        token (-> (key ephemeral) (make-n-bytes 16) bytes->SHA256 (make-n-bytes 16))
+        seq (rand-int Integer/MAX_VALUE)]
+    (-> ephemeral
+        (assoc :endpoint endpoint)
+        (assoc :token token)
+        (assoc :seq seq))))
+
 (defmethod key "1a" [cs]
-  (-> (:keypair cs) .getPublic .getQ (.getEncoded true) Hex/encodeHexString))
+  (-> (:public cs) .getQ (.getEncoded true)))
 
 (defmethod secret "1a" [cs]
-  (-> (:keypair cs) .getPrivate .getD .toByteArray
-                    (make-n-bytes 20) Hex/encodeHexString))
+  (-> (:private cs) .getD .toByteArray
+                    (make-n-bytes 20)))
+
+(defn- bytes->1a-message [bytes]
+  (if (< (count bytes) (+ 21 4 4)) nil
+      (let [key (byte-array (take 21 bytes))
+            iv (byte-array (->> bytes (drop 21) (take 4)))
+            inner (byte-array (->> bytes (drop (+ 21 4)) (drop-last 4)))
+            hmac (byte-array (->> bytes (take-last 4)))]
+        {:key  key
+         :iv iv
+         :inner inner
+         :hmac  hmac})))
+
+(defonce ivz-12 (byte-array (repeat 12 0x00)))
+
+(defmethod decrypt "1a" [local msgbuf]
+  (if-let [message (bytes->1a-message msgbuf)]
+    (let [shared-secret (secp160r1-shared-secret (secret local) (:key message))
+          key (fold (bytes->SHA256 shared-secret) 1)
+          padded-iv (byte-array (concat (:iv message) ivz-12))
+          decrypted (AES-128-CTR-decrypt key padded-iv (:inner message))]
+      [local decrypted])
+    [local nil]))
+
+(defmethod encrypt "1a" [local remote msgbuf]
+  (let [{:keys [endpoint]} remote
+        shared-secret (secp160r1-shared-secret (secret remote) endpoint)
+        aes-key (fold (bytes->SHA256 shared-secret) 1)
+        new-seq (inc (:seq remote))
+        iv (int->bytes new-seq)
+        padded-iv (byte-array (concat iv ivz-12))
+        encrypted (AES-128-CTR-encrypt aes-key padded-iv msgbuf)
+        mac-secret (secp160r1-shared-secret (secret local) endpoint)
+        macd (byte-array (concat (key remote) iv encrypted))
+        hmac (fold (HMAC-SHA256 (byte-array (concat mac-secret iv)) macd) 3)]
+    [(assoc local :seq new-seq)
+     remote
+     (byte-array (concat macd hmac))]))
--- test/clj/telehash/e3x_test.clj
+++ test/clj/telehash/e3x_test.clj
@@ -4,31 +4,31 @@
             [clojure.test :refer :all]))
 
 (def cs1a "1a")
-(def cs1a-test-1 {:key "03be277f53630a084de2f39c7ff9de56c38bb9d10c"
-                  :secret "792fd655c8e03ae16e0e49c3f0265d04689cbea3"})
+(def cs1a-A {:key "03be277f53630a084de2f39c7ff9de56c38bb9d10c"
+             :secret "792fd655c8e03ae16e0e49c3f0265d04689cbea3"})
 
-(def cs1a-test-2 {:key "0365694904381c00dfb7c01bb16b0852ea584a1b0b"
-                  :secret "031b502b0743b80c1575f4b459792b5d76ad636d"})
+(def cs1a-B {:key "0365694904381c00dfb7c01bb16b0852ea584a1b0b"
+             :secret "031b502b0743b80c1575f4b459792b5d76ad636d"})
+
+(def A->B (hex->bytes "030d8def4405c1380afeca3760322be710a3f53cfe7c9bed207249f31af977"))
+(def B->A (hex->bytes "021aaad76e86b2c951a0ab00b22d031567b6bd556aa953a22b65f5d62dcbba"))
 
 (defn validate-generated-cs1a [keypair]
-  (is (= 42 (-> keypair key count)))
-  (is (= 40 (-> keypair secret count))  (str "SECRET: " (:secret keypair))))
+  (is (= 21 (-> keypair key count)))
+  (is (= 20 (-> keypair secret count))  (str "SECRET: " (:secret keypair))))
 
-(deftest generated-cs1a-key-secret-validation
-  (doseq [generated (repeatedly 100 #(generate-cs cs1a))]
+(deftest generated-cs1a-identity-validation
+  (doseq [generated (repeatedly 100 #(generate-local cs1a))]
     (validate-generated-cs1a generated))
   )
 
 (deftest generated-cs1a-id-check
-  (is (= "1a" (:id (generate-cs "1a")))))
+  (is (= "1a" (:id (generate-local "1a")))))
 
 (deftest valid-local-cs1a-should-load-correctly
-  (let [cs-1 (load-local "1a"  cs1a-test-1)
-        cs-2 (load-local "1a" cs1a-test-2)]
-    (is (= (key cs-1) (:key cs1a-test-1)))
-    (is (= (secret cs-1) (:secret cs1a-test-1)))
-    (is (= (key cs-2) (:key cs1a-test-2)))
-    (is (= (secret cs-2) (:secret cs1a-test-2)))))
+  (let [cs-1 (load-local "1a"  cs1a-A)]
+    (is (= (-> cs-1 key bytes->hex) (:key cs1a-A)))
+    (is (= (-> cs-1 secret bytes->hex) (:secret cs1a-A)))))
 
 (deftest loading-invalid-local-cs1a-should-throw-error
   (let [empty {}
@@ -45,3 +45,50 @@
     (is (thrown? Exception (load-local "1a" short-key)))
     (is (thrown? Exception (load-local "1a" long-secret)))
     (is (thrown? Exception (load-local "1a" long-key)))))
+
+(deftest generated-cs1a-ephemeral-validation
+  (let [ephemeral (generate-ephemeral "1a")]
+    (is (= 21 (count (key ephemeral))))))
+
+(deftest should-local-decrypt-cs1a
+  (let [local (load-local "1a" cs1a-A)
+        [_ decrypted] (decrypt local B->A)]
+    (is (= 2 (count decrypted)))
+    (is (= "0000" (bytes->hex decrypted)))))
+
+(deftest should-load-remote-cs1a
+  (let [remote (load-remote "1a" cs1a-B)]
+    (is (= 16 (-> remote :token count)))))
+
+(deftest should-local-encrypt-cs1a
+  (let [local (load-local "1a" cs1a-A)
+        remote (load-remote "1a" cs1a-B)
+        [_ _ message] (encrypt local remote (hex->bytes "0000"))]
+    (is (= 31 (count message)))))
+
+(deftest should-remote-encrypt-cs1a
+  (let [local (load-local "1a" cs1a-B)
+        remote (load-remote "1a" cs1a-A)
+        [_ _ message] (encrypt local remote (hex->bytes "0000"))]
+    (is (= 31 (count message)))))
+
+(deftest AES-128-encrypt-decrypt
+  (let [in (hex->bytes "0000")
+        shared1 (secp160r1-shared-secret (hex->bytes (:secret cs1a-A))
+                                         (hex->bytes (:key cs1a-B)))
+        shared2 (secp160r1-shared-secret (hex->bytes (:secret cs1a-B))
+                                         (hex->bytes (:key cs1a-A)))
+        key (fold (bytes->SHA256 shared1) 1)
+        iv (byte-array (repeat 16 0x00))
+        encrypted (AES-128-CTR-encrypt key iv in)
+        out (AES-128-CTR-decrypt key iv encrypted)]
+    (is (= (bytes->hex shared1) (bytes->hex shared2)))
+    (is (= (bytes->hex in) (bytes->hex out)))))
+
+(deftest encrypt-decrypt-round-trip
+  (let [local-A (load-local "1a" cs1a-A)
+        remote-B (load-remote "1a" cs1a-B)
+        local-B (load-local "1a" cs1a-B)
+        [_ _ encrypted] (encrypt local-A remote-B (hex->bytes "0000"))
+        [_ decrypted] (decrypt local-B encrypted)]
+    (is (= "0000" (bytes->hex decrypted)))))
